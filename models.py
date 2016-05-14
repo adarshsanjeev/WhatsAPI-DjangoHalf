3d9067bd7445110351449742bada70f29e61ee4a
Adarsh <adarshsanjeev@gmail.com>
2016-05-14 07:25:14 +0300

    Added Login and Register

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django import forms
 
-# Create your models here.
+class UserForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- views.py
+++ views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.forms.widgets import HiddenInput
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import UserForm
 
-# Create your views here.
+# import webwhatsapp
+
+@login_required(login_url="/WhatsAPI/login/")
+def index(request):
+    return render(request, "index.html", {})
+
+def user_login(request):
+    form_errors = None
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(username, password)
+            return HttpResponseRedirect('/WhatsAPI')
+        else:
+            form_errors = "Username or password is incorrect"
+    return render(request, 'login.html', {'form_errors':form_errors}) 
+   
+def user_register(request):
+    if request.POST:
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            return HttpResponseRedirect('/WhatsAPI')
+    else:
+        user_form = UserForm()
+    return render(request, 'register.html', {'user_form':user_form})
+
