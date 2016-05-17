3dae5966b750685089cc07be17a0b86a3edd1381
Adarsh <adarshsanjeev@gmail.com>
2016-05-18 01:01:48 +0300

    Updated API

--- urls.py
+++ urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^login/$', views.user_login, name="login"),
     url(r'^register/$', views.user_register, name="register"),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
